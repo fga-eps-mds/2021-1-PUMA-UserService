c2c7c32876a8aaf6b783e6a008b49cf2f56ffbbd
Nicolas Mantzos <georgeos.nicolas83@gmail.com>
2021-10-20 10:21:10 -0300

    [#85] implementation of subjectRoutes (post, get and getById)

--- src/repository/subjectRepository.js
+++ src/repository/subjectRepository.js
@@ -11,7 +11,46 @@ function getSubjects() {
     });
 }
 
+function addSubject(subject) {
+    return new Promise((resolve, reject) => {
+        let resp;
+        switch (subject.operacao){
+            case 'alteracao':
+                resp = db.query(
+                    'UPDATE SUBJECT SET name = $1, coursesyllabus = $2 WHERE subjectid = $3 RETURNING *',
+                    [subject.name, subject.coursesyllabus, subject.subjectid]
+                )
+                break;
+            case 'cadastro':
+                resp = db.query(
+                    'INSERT INTO SUBJECT(name,coursesyllabus) VALUES ($1,$2) RETURNING *',
+                    [subject.name, subject.coursesyllabus]
+                )
+                break;
+        }
+        resp.then((response) => {
+            resolve(response.rows);
+        }).catch((response) => {
+            reject(response);
+        });
+    });
+}
+
+function getSubject(subjectIdParam) {
+    return new Promise((resolve, reject) => {
+        db.query(
+            'SELECT * FROM SUBJECT as s WHERE subjectid=$1',
+            [subjectIdParam],
+        ).then((response) => {
+                resolve(response.rows);
+        }).catch((response) => {
+                reject(response);
+        });
+    });
+}
 
 module.exports = {
-    getSubjects
+    getSubjects,
+    addSubject,
+    getSubject
 };
